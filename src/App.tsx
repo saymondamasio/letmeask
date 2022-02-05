a21e80387360e83839dfa0ccbac90c7d15aba421
Saymon Damásio <saymon.damasio95@gmail.com>
2022-02-05 11:36:13 -0300

    feat: add function create room

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Toaster } from 'react-hot-toast'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
+import { AdminRoom } from './pages/AdminRoom'
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
+import { Room } from './pages/Room'
 
 export function App(): JSX.Element {
   return (
@@ -9,8 +12,11 @@ export function App(): JSX.Element {
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/rooms/new" element={<NewRoom />} />
+          <Route path="rooms/new" element={<NewRoom />} />
+          <Route path="rooms/:id" element={<Room />} />
+          <Route path="admin/rooms/:id" element={<AdminRoom />} />
         </Routes>
+        <Toaster />
       </AuthProvider>
     </BrowserRouter>
   )
--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,10 +1,38 @@
-import { Link } from 'react-router-dom'
+import { push, ref, set } from 'firebase/database'
+import { FormEvent, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import illustration from '../../assets/images/illustration.svg'
 import logo from '../../assets/images/logo.svg'
 import { Button } from '../../components/Button'
+import { useAuth } from '../../hooks/useAuth'
+import { database } from '../../services/firebase'
 import styles from './styles.module.scss'
 
 export function NewRoom() {
+  const [name, setName] = useState('')
+
+  const { user } = useAuth()
+  const navigate = useNavigate()
+
+  async function handleCreateRoom(e: FormEvent) {
+    e.preventDefault()
+
+    if (name.trim() === '') {
+      return
+    }
+
+    const roomsRef = ref(database, 'rooms')
+
+    const newRoomRef = await push(roomsRef)
+
+    set(newRoomRef, {
+      title: name,
+      authorId: user?.id,
+    })
+
+    navigate(`/rooms/${newRoomRef.key}`)
+  }
+
   return (
     <div className={styles.container}>
       <aside>
@@ -20,8 +48,14 @@ export function NewRoom() {
         <div className={styles.mainContent}>
           <img src={logo} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
-          <form>
-            <input type="text" placeholder="Nome da sala" />
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              name="name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
