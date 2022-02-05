c6ef2005d1ce6e5669b03c6746966bb3bfa92cd6
Saymon Damásio <saymon.damasio95@gmail.com>
2022-02-05 11:35:18 -0300

    feat: create button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,16 @@
 import { ButtonHTMLAttributes } from 'react'
 import styles from './styles.module.scss'
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement>
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean
+}
 
-export function Button({ ...rest }: Props): JSX.Element {
-  return <button className={styles.container} type="button" {...rest} />
+export function Button({ isOutlined = false, ...rest }: Props): JSX.Element {
+  return (
+    <button
+      className={`${styles.container} ${isOutlined ? styles.outlined : ''}`}
+      type="button"
+      {...rest}
+    />
+  )
 }
